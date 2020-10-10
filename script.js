14c1453f3c6e586ad9084617411123cfd361ce4b
James Hernandez <james@j-hernandez.com>
2020-10-10 11:33:05 -0400

    callback hell refactor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const id = 'j-hernandez';
 
 function updateUI(info) {
-    console.log('ui updated');
+    document.getElementById('weather').innerHTML = JSON.stringify(info);
 }
 
 function showError(error) {
@@ -12,22 +12,27 @@ function getLocationURL(locationArray) {
     return `https://api.openweathermap.org/data/2.5/forecast?q=${locationArray[0]},${locationArray[1]},USA&APPID=2f66e52e225a3d7ce8bf18dd58e5a6fc`;
 }
 
-
-
-document.getElementById('get-user').addEventListener('click', function() {
+function getUser(id, successCallback, failureCallback) {
     $.getJSON({
         url: `https://api.github.com/users/${id}`,
-        success: function(info) {
-            document.getElementById('user').innerHTML = JSON.stringify(info);
-            $.getJSON({
-                url: getLocationURL(info.location.split(', ')),
-                success: function(weather) {
-                    document.getElementById('weather').innerHTML = JSON.stringify(weather);
-                },
-                error: showError
-            })
-        },
-        error: showError,
+        success: successCallback,
+        error: failureCallback
     })
+}
+
+function getWeather(location, successCallback, failureCallback) {
+    $.getJSON({
+        url: getLocationURL(location),
+        success: successCallback,
+        error: failureCallback
+    })
+}
+
+document.getElementById('get-user').addEventListener('click', function() {
+    getUser('j-hernandez', function(userInfo) {
+        getWeather(userInfo.location.split(', '), function(weather) {
+            updateUI(weather)
+        }, showError)
+    }, showError)
 });
 
