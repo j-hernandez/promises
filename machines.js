dc07a02168546aa51e56ea04c97a310c8da71980
James Hernandez <james@j-hernandez.com>
2020-10-10 10:38:07 -0400

    machines discussion

--- machines.js
+++ machines.js
@@ -1,5 +1,27 @@
-function add(x, y) {
+function add (x, y) {
     return x + y;
 }
 
-add(2,3);
+function subtract(x, y) {
+    return x - y;
+}
+
+function higherOrderFunction(x, y, callback) { // x is a number, and addReference is a function
+    return callback(x, y);
+}
+
+console.log(higherOrderFunction(5, 5, add)); // Should return 10
+//console.log(higherOrderFunction(20, 15, subtract)); // Should return 5
+
+
+
+/*
+let me = add;
+let you = add;
+let someoneElse = add;
+
+console.log(me(2, 3));
+console.log(you(2, 3));
+console.log(someoneElse(2,3));
+*/
+
