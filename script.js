70c43ae10486863ccd8224f2a77ca705dc635706
James Hernandez <james@j-hernandez.com>
2020-10-10 13:25:14 -0400

    promise refactor complete

--- script.js
+++ script.js
@@ -9,14 +9,20 @@ function showError(error) {
 }
 
 function getLocationURL(locationArray) {
-    return `https://api.openweathermap.org/data/2.5/forecast?q=${locationArray[0]},${locationArray[1]},USA&APPID=2f66e52e225a3d7ce8bf18dd58e5a6fc`;
+    return `https://api.openweakkkksdlskthermap.org/data/2.5/forecast?q=${locationArray[0]},${locationArray[1]},USA&APPID=2f66e52e225a3d7ce8bf18dd58e5a6fc`;
 }
 
 function getUser(id) {
     return new Promise(function (resolve, reject) {
         $.getJSON({
             url: `https://api.github.com/users/${id}`,
-            success: resolve,
+            success: function(userInfo) {
+                // This is actually getWeather()
+                // and we're passing in the location
+                // from the user object so that 
+                // we can look it up successfully
+                resolve(userInfo.location.split(', '));
+            },
             error: reject
         })
     })
@@ -34,20 +40,16 @@ function getWeather(location) {
 
 // Click handler 
 document.getElementById('get-user').addEventListener('click', function() {
-    // Lets work with our new getUser function which returns a promise
-    const userPromise = getUser('j-hernandez');
-    // User Promise has been fulfilled
-    userPromise.then(function (userInfo) {
-        const weatherPromise = getWeather(userInfo.location.split(', '));
-        // We have user data, and we have weather data at this point
-        weatherPromise.then(function(weather) {
+    // 
+    getUser('j-hernandez')
+        // I've got data from github, lets pass it to getWeather
+        .then(getWeather) // getWeather(location)
+        // I've got weather data, now update the UI
+        .then(function (weather) {
             updateUI(weather);
-        });
-        // We have user data, but our weather request failed
-        weatherPromise.catch(showError);
-    });
-    // User Promise has been rejected
-    userPromise.catch(showError);
+        })
+        // Any errors that show up in the above functions will get caught here
+        .catch(showError);
 });
 
 
